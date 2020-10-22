16943e62681ad7bdce32852f0084bb982f57c969
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-10-22 22:32:49 +0200

    init repo

---
+++ TemplatedData/__init__.py
@@ -0,0 +1,47 @@
+import re
+from robot.libraries.BuiltIn import BuiltIn
+from jinja2 import Environment, BaseLoader
+
+
+class TemplatedData:
+    def __init__(self):
+        self.built_in = BuiltIn()
+        self.robot_var_pattern = re.compile(r'\${[^}]+}')
+
+    @staticmethod
+    def normalize(value):
+        return value.lower()
+
+    def get_templated_data(self, template, default_empty='', jinja_template=False, **kwargs):
+        vars = self.robot_var_pattern.findall(template)
+        overwrite_values = {arg: self.normalize(value) for arg, value in kwargs.items()}
+        templated_vars = {}
+        for var in vars:
+            print(var)
+            name, *default = var[2:-1].split(':', maxsplit=1)
+            default = default[0] if default else default_empty
+            raw_name, *attrs = name.split('.', maxsplit=1)
+            default = self.built_in.get_variable_value(f'${{{raw_name}}}', default)
+            default = overwrite_values.get(raw_name, default)
+            templated_vars[raw_name] = default
+            print(default)
+            if jinja_template:
+                var_replaced = f'templated_vars["{raw_name}"]'
+                if attrs:
+                    var_replaced += '.' + attrs[0]
+            else:
+                var_replaced = str(default)
+            template = template.replace(var, var_replaced)
+        print(template)
+        r_template = Environment(loader=BaseLoader()).from_string(template)
+        return r_template.render(templated_vars=templated_vars)
+
+    def get_templated_data_from_path(self, path, default_empty='', jinja_template=False, **kwargs):
+        with open(path) as f:
+            template = f.read()
+        return self.get_templated_data(template, default_empty, jinja_template, **kwargs)
+
+
+if __name__ == "__main__":
+    templated_data = TemplatedData()
+    templated_data.get_templated_data_from_path(r'..\templated_data_jinja.json', jinja_template=True)
---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+from robotclean.__version__ import __version__
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.rst").read_text()
+CLASSIFIERS = """
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Framework :: Robot Framework :: Tool
+Topic :: Software Development :: Quality Assurance
+Topic :: Utilities
+""".strip().splitlines()
+
+setup(
+    name='robotframework-templateddata',
+    version=__version__,
+    description='Robot Framework library for generating test data from templates',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/bhisz/robotframework-templateddata",
+    author="Bartlomiej Hirsz",
+    author_email="bartek.hirsz@gmail.com",
+    license="Apache License 2.0",
+    platforms="any",
+    classifiers=CLASSIFIERS,
+    packages=['TemplatedData'],
+    include_package_data=True,
+    install_requires=['robotframework>=3.2.1', 'jinja2']
+)
