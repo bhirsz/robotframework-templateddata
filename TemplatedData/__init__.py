cbef2590f1bdf535cf39d01c48df4d18899a634a
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-10-22 22:43:01 +0200

    improve logging:

--- TemplatedData/__init__.py
+++ TemplatedData/__init__.py
@@ -1,5 +1,6 @@
 import re
 from robot.libraries.BuiltIn import BuiltIn
+from robot.api import logger
 from jinja2 import Environment, BaseLoader
 
 
@@ -13,18 +14,18 @@ class TemplatedData:
         return value.lower()
 
     def get_templated_data(self, template, default_empty='', jinja_template=False, **kwargs):
+        logger.debug('Template:')
+        logger.debug(template)
         vars = self.robot_var_pattern.findall(template)
         overwrite_values = {arg: self.normalize(value) for arg, value in kwargs.items()}
         templated_vars = {}
         for var in vars:
-            print(var)
             name, *default = var[2:-1].split(':', maxsplit=1)
             default = default[0] if default else default_empty
             raw_name, *attrs = name.split('.', maxsplit=1)
             default = self.built_in.get_variable_value(f'${{{raw_name}}}', default)
             default = overwrite_values.get(raw_name, default)
             templated_vars[raw_name] = default
-            print(default)
             if jinja_template:
                 var_replaced = f'templated_vars["{raw_name}"]'
                 if attrs:
@@ -32,12 +33,14 @@ class TemplatedData:
             else:
                 var_replaced = str(default)
             template = template.replace(var, var_replaced)
-        print(template)
         if jinja_template:
             r_template = Environment(loader=BaseLoader()).from_string(template)
-            return r_template.render(templated_vars=templated_vars)
+            replaced_data = r_template.render(templated_vars=templated_vars)
         else:
-            return template
+            replaced_data = template
+        logger.debug('Rendered template:')
+        logger.debug(replaced_data)
+        return replaced_data
 
     def get_templated_data_from_path(self, path, default_empty='', jinja_template=False, **kwargs):
         with open(path) as f:
