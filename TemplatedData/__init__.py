1b84f3a942792e4e91642dd86a7cad41e0e08087
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-10-23 09:20:34 +0200

    minor fixes

--- TemplatedData/__init__.py
+++ TemplatedData/__init__.py
@@ -22,10 +22,9 @@ class TemplatedData:
         jinja_template = kwargs.pop('jinja_template', self.jinja_template)
         return_type = kwargs.pop('return_type', self.return_type)
         logger.debug(f'Template:\n{template}')
-        robot_vars = self.robot_var_pattern.findall(template)
         overwrite_values = {arg: self.normalize(value) for arg, value in kwargs.items()}
         templated_vars = {}
-        for var in robot_vars:
+        for var in self.robot_var_pattern.findall(template):
             name, *default = var[2:-1].split(':', maxsplit=1)
             default = default[0] if default else default_empty
             raw_name, *attrs = name.split('.', maxsplit=1)
@@ -54,7 +53,6 @@ class TemplatedData:
 
     @staticmethod
     def return_data_with_type(data, data_type):
-        if data_type == 'text':
-            return data
         if data_type == 'json':
             return json.loads(data)
+        return data
